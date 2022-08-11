4950c6a54a30a8e056eca463e1e9d878ea0fbc23
Valentine <okosival@gmail.com>
2022-08-11 06:53:23 +0100

    deployed contracted successsfully

---
+++ config.js
@@ -0,0 +1,3 @@
+
+  export const marketplaceAddress = "0x320eC7F56991e3120f24111E91bb8ea83aa6699F"
+  
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,26 @@
-require('@nomicfoundation/hardhat-chai-matchers');
+/* hardhat.config.js */
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-	solidity: '0.8.9',
+	defaultNetwork: 'hardhat',
 	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+
 		goerli: {
-			url: 'https://eth-goerli.g.alchemy.com/v2/l81H09zhtSClnJY-Ao7PsTevtAoZMQfJ',
-			accounts: [
-				'f872af8e24689d2b72bf17d97e91a200e703e428bc97aeb5c18fa2f19169d60b',
-			],
+			url: 'https://eth-goerli.g.alchemy.com/v2/O_IzHiXU8kgW1DQn6FWJN2qFXcSPyMvr',
+			accounts: [process.env.ALCHEMY_URL],
+		},
+	},
+	solidity: {
+		version: '0.8.9',
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
 		},
 	},
 };
