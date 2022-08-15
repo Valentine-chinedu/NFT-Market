edf703a85a0e92743cc698b6219fbe854eb04d41
Valentine <okosival@gmail.com>
2022-08-15 20:23:27 +0100

    header design done

---
+++ components/Header.jsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const Header = () => {
+	const router = useRouter();
+
+	return (
+		<div className='flex border-b items-center justify-around bg-gradient-to-br h-20 from-rose-900 to-gray-900'>
+			<p className='text-3xl font-bold text-gray-100'>NFT Marketplace</p>
+			<nav className=''>
+				<div className='flex font-semibold space-x-8'>
+					<Link href='/'>
+						<a
+							className={`text-pink-500 hover:text-pink-300 ${
+								router.pathname === '/' && 'border-b border-pink-300'
+							}`}
+						>
+							Buy NFTs
+						</a>
+					</Link>
+
+					<Link href='/MyNfts'>
+						<a
+							className={`text-pink-500 hover:text-pink-300 ${
+								router.pathname === '/MyNfts' && 'border-b border-pink-300'
+							}`}
+						>
+							My NFTs
+						</a>
+					</Link>
+					<Link href='/DashBoard'>
+						<a
+							className={`text-pink-500 hover:text-pink-300 ${
+								router.pathname === '/DashBoard' && 'border-b border-pink-300'
+							}`}
+						>
+							Dashboard
+						</a>
+					</Link>
+				</div>
+			</nav>
+			<Link href='/CreateNft'>
+				<a className=' rounded-3xl px-4 py-1 text-sm font-semibold bg-pink-500 text-gray-100'>
+					MINT NFT
+				</a>
+			</Link>
+		</div>
+	);
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,11 @@
 import '../styles/globals.css';
 import Link from 'next/link';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<div>
-			<nav className='border-b p-6'>
-				<p className='text-4xl font-bold'>Metaverse Marketplace</p>
-				<div className='flex mt-4'>
-					<Link href='/'>
-						<a className='mr-4 text-pink-500'>Home</a>
-					</Link>
-					<Link href='/CreateNft'>
-						<a className='mr-6 text-pink-500'>Sell NFT</a>
-					</Link>
-					<Link href='/MyNfts'>
-						<a className='mr-6 text-pink-500'>My NFTs</a>
-					</Link>
-					<Link href='/DashBoard'>
-						<a className='mr-6 text-pink-500'>Dashboard</a>
-					</Link>
-				</div>
-			</nav>
+			<Header />
 			<Component {...pageProps} />
 		</div>
 	);
